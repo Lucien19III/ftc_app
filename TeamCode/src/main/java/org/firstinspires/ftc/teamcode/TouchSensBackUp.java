8188e336b3b8b2a44de7b8f9ce413bbf760a0ce3
Lucien19III <ocs.lucien.anderson@gmail.com>
2016-10-25 15:07:29 -0700

    Lucien Test codes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TouchSensBackUp.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+/**
+ * Created by mgreg on 10/20/2016.
+ */
+
+public class TouchSensBackUp extends LinearOpMode {
+        DcMotor leftMotor;
+        DcMotor rightMotor;
+        TouchSensor touchSensor;
+
+        int BACKUP_TIME = 1000;
+        int TURN_TIME = 2000;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        leftMotor = hardwareMap.dcMotor.get("motor_left");
+        rightMotor = hardwareMap.dcMotor.get("motor_right");
+
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        touchSensor = hardwareMap.touchSensor.get("sensor_touch");
+
+        waitForStart();
+
+        while (true) {
+            if (touchSensor.isPressed()) {
+                leftMotor.setPower(-0.25);
+                rightMotor.setPower(-0.25);
+                telemetry.addData("state", "backing_up");
+                sleep(BACKUP_TIME);
+
+            leftMotor.setPower(0.30);
+            rightMotor.setPower(-0.30);
+            telemetry.addData("state", "turning");
+                sleep(TURN_TIME);
+            } else {
+                leftMotor.setPower(0.5);
+                rightMotor.setPower(0.5);
+                telemetry.addData("state", "driving");
+            }
+
+
+            waitOneFullHardwareCycle();
+        }
+    }
+}
+
+
