c9387fe4f5f35f9f0b48e461c3f416a5bbf1b098
Lucien19III <ocs.lucien.anderson@gmail.com>
2016-11-17 15:47:54 -0800

    Lucien Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FindLine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FindLine.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
@@ -17,7 +17,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @Autonomous
 //@Disabled   // comment out or remove this line to enable this opmode
-public class FindLine extends OpMode {
+public class FindLine extends LinearOpMode {
     DcMotor motorRight;
     DcMotor motorLeft;
     Servo armLeft;
@@ -26,10 +26,6 @@ public class FindLine extends OpMode {
     ModernRoboticsI2cRangeSensor rangeSensor;
     ElapsedTime time;
     ColorSensor colorSensor;
-
-    double distance;
-    double reflectance;
-    double refVar = 0.25;
 
     //static final double forwardTime = 1.0;
 
@@ -44,26 +40,25 @@ public class FindLine extends OpMode {
 
 
     @Override
-    public void init() {
+    public void runOpMode() {
 
         // get a reference to our compass
         rangeSensor = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "sensor_range");
-
+        double distance = rangeSensor.cmOptical();
         motorLeft = hardwareMap.dcMotor.get("motor_left");
         motorRight = hardwareMap.dcMotor.get("motor_right");
         colorSensor = hardwareMap.colorSensor.get("sensor_color");
         armLeft = hardwareMap.servo.get("arm_left");
         armRight = hardwareMap.servo.get("arm_right");
         motorRight.setDirection(com.qualcomm.robotcore.hardware.DcMotor.Direction.REVERSE);
+
         opticalDistanceSensor = hardwareMap.opticalDistanceSensor.get("sensor_EOPD");
+        double reflectance = opticalDistanceSensor.getLightDetected();
 
-    }
-
-    @Override
-    public void loop () {
-
-        distance = rangeSensor.cmOptical();
-        reflectance = opticalDistanceSensor.getLightDetected();
+        double refVar = 0.25;
+        time = new ElapsedTime();
+        // wait for the start button to be pressed
+        waitForStart();
 
         switch (state) {
             case ONE:
@@ -71,7 +66,7 @@ public class FindLine extends OpMode {
             while (reflectance < refVar) {
                 motorRight.setPower(0.5);
                 motorLeft.setPower(0.5);
-                //look at the telemetry
+//look at the telemetry
                 telemetry.addData("raw ultrasonic", rangeSensor.rawUltrasonic());
                 telemetry.addData("raw optical", rangeSensor.rawOptical());
                 telemetry.addData("cm optical", "%.2f cm", rangeSensor.cmOptical());
@@ -100,15 +95,15 @@ public class FindLine extends OpMode {
                 case SENSE_COLOR:
                     telemetry.addData("State", state);
                     if (colorSensor.blue() > 5) {
-                        armLeft.setPosition(0);
-                        motorRight.setPower(0.5);
-                        motorLeft.setPower(0.5);
-                        //Need to sleep for 1500 nanoseconds
+                    armLeft.setPosition(0);
+                    motorRight.setPower(0.5);
+                    motorLeft.setPower(0.5);
+                    sleep(1500);
                     } else {
                         armRight.setPosition(0);
                         motorLeft.setPower(0.5);
                         motorRight.setPower(0.5);
-                        //Need to sleep for 1500 nanoseconds
+                        sleep(1500);
                     }
                     state = state.EXIT;
                     break;
