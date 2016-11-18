688ac4f43266d199989ffe472d06136abf38a155
Cole Smith <cole126699@gmail.com>
2016-11-17 17:19:45 -0800

    -_-

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/ColeOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/ColeOpMode.java
@@ -1,8 +1,25 @@
 package org.firstinspires.ftc.teamcode.TestCode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
 /**
  * Created by CJS on 11/17/16.
  */
 
+
+@Autonomous
 public class ColeOpMode {
+
+
+    public void runOpMode () {
+
+
+
+
+
+
+
+
+    }
+
 }
