6418eab96142cb3054d8bd9d0c1710f8d5d1036f
mckelveygreg <mckelveygreg@gmail.com>
2016-11-18 22:58:56 -0800

    commented out unnessecary code Started encoder method (Use their template code!!)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderCalibrate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderCalibrate.java
@@ -26,38 +26,39 @@ public class EncoderCalibrate extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-   {
-        arm = hardwareMap.dcMotor.get("arm");
-        pullBack = hardwareMap.dcMotor.get("pullBack");
+        {
+            arm = hardwareMap.dcMotor.get("arm");
+            pullBack = hardwareMap.dcMotor.get("pullBack");
 
-        telemetry.addData("Status", "Resetting Encoders");    //
-        telemetry.update();
+            telemetry.addData("Status", "Resetting Encoders");    //
+            telemetry.update();
 
-        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        pullBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-
-        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        pullBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        // Send telemetry message to indicate successful Encoder reset
-        telemetry.addData("Path0",  "Starting at %7d :%7d",
-                arm.getCurrentPosition(),
-                pullBack.getCurrentPosition());
-        telemetry.update();
-
-       // Wait for the game to start (driver presses PLAY)
-       waitForStart();
-
-       encoder
-
-        telemetry.addData("arm position", arm.getCurrentPosition());
-        telemetry.addData("pull Back position", pullBack.getCurrentPosition());
-
-        arm.setPower(gamepad1.left_stick_y);
-        pullBack.setPower(gamepad1.right_stick_y);
+            arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            pullBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
 
-        telemetry.update();
+            arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            pullBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            // Send telemetry message to indicate successful Encoder reset
+            telemetry.addData("Path0", "Starting at %7d :%7d",
+                    arm.getCurrentPosition(),
+                    pullBack.getCurrentPosition());
+            telemetry.update();
+
+            // Wait for the game to start (driver presses PLAY)
+            waitForStart();
+
+            //encoder
+
+            telemetry.addData("arm position", arm.getCurrentPosition());
+            telemetry.addData("pull Back position", pullBack.getCurrentPosition());
+
+            arm.setPower(gamepad1.left_stick_y);
+            pullBack.setPower(gamepad1.right_stick_y);
+
+
+            telemetry.update();
+        }
     }
 }
