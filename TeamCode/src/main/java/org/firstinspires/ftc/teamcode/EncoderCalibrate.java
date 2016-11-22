913e90be7c037aeb8724590e8cb54204786d5d69
Cole Smith <cole126699@gmail.com>
2016-11-21 17:44:16 -0800

    Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderCalibrate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderCalibrate.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by mgreg on 11/18/2016.
@@ -13,15 +14,19 @@ public class EncoderCalibrate extends LinearOpMode {
     DcMotor arm;
     DcMotor pullBack;
 
+    private ElapsedTime runtime = new ElapsedTime();
     //neverest 60:1
     final static int ENCODER_CPR = 420;
     final static double GEAR_RATIO = 1;
     final static double REEL_DIAMETER = 2.1; //cm
-    final static double DRAW = 12;  // change this to change the drawback distance
+    //final static double DRAW = 12;  // change this to change the drawback distance
 
     final static double CIRCUMFERENCE = Math.PI * REEL_DIAMETER;
-    final static double ROTATIONS = DRAW / CIRCUMFERENCE;
-    final static double COUNTS = ENCODER_CPR * ROTATIONS * GEAR_RATIO;
+    //final static double ROTATIONS = DRAW / CIRCUMFERENCE;
+    final static double COUNTS_PER_CM = ENCODER_CPR * GEAR_RATIO / CIRCUMFERENCE;
+
+    static final double PULL_SPEED = 0.6;
+    static final double RELEASE_SPEED = 0.5;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -49,7 +54,8 @@ public class EncoderCalibrate extends LinearOpMode {
             // Wait for the game to start (driver presses PLAY)
             waitForStart();
 
-            //encoder
+            lockNload(PULL_SPEED, 20, 2.0);
+            lockNload(RELEASE_SPEED, 20, 3.0);
 
             telemetry.addData("arm position", arm.getCurrentPosition());
             telemetry.addData("pull Back position", pullBack.getCurrentPosition());
@@ -59,6 +65,44 @@ public class EncoderCalibrate extends LinearOpMode {
 
 
             telemetry.update();
+        }
+    }
+
+    public void lockNload(double speed,
+                          double pullBackCM,
+                          double timeoutS) {
+        int newPullBackTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newPullBackTarget = pullBack.getCurrentPosition() + (int) (pullBackCM * COUNTS_PER_CM);
+            pullBack.setTargetPosition(newPullBackTarget);
+
+
+            // Turn On RUN_TO_POSITION
+            pullBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            pullBack.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (pullBack.isBusy())) {
+                // Display it for the driver.
+                telemetry.addData("Path1", "Running to %7d :%7d", newPullBackTarget);
+                telemetry.addData("Path2", "Running at %7d :%7d",
+                        pullBack.getCurrentPosition());
+                telemetry.update();
+            }
+            // Stop all motion;
+            pullBack.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            pullBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
     }
 }
